c90bc422ae37d2a0a9ad99f5da170fd706ab270c
leobrowaldh <leobrowaldh@gmail.com>
2024-09-24 11:10:21 +0200

    Add project files.

---
+++ WebPushNotificationApp.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebPushNotificationApp.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
