763ac02d317a22fff6c824f24efdb05dfe199aa2
leobrowaldh <leobrowaldh@gmail.com>
2024-09-25 10:13:49 +0200

    service worker created, icon and badges missing

---
+++ WebPushNotificationApp.Mvc/wwwroot/js/sw.js
@@ -0,0 +1,11 @@
+﻿self.addEventListener('push', function (event) {
+    const data = event.data.json();
+    const options = {
+        body: data.body,
+        icon: '/path/to/icon.png',
+        badge: '/path/to/badge.png'
+    };
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
