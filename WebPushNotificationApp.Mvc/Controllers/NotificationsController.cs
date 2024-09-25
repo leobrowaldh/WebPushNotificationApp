28d9221bc4fa0adec9b5d11b8b2c559062699737
fredrik nellbeck <Foggel@live.se>
2024-09-25 07:56:42 +0200

    Remove branch

---
+++ WebPushNotificationApp.Mvc/Controllers/NotificationsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebPush;
+
+namespace WebPushNotificationApp.Mvc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationsController : ControllerBase
+    {
+        private readonly PushService _pushService;
+
+        public NotificationsController(PushService pushService)
+        {
+            _pushService = pushService;
+        }
+
+        //Handels the subscription
+        [HttpPost("surbscribe")]
+        public IActionResult Subscribe([FromBody] PushSubscription subscription)
+        {
+            // Store the subscritpion for later use
+            return Ok();
+        }
+
+        // Sends the notification
+        [HttpPost("notify")]
+        public async Task<IActionResult> Notify()
+        {
+            var subscription = new PushSubscription
+            {
+                Endpoint = "https://fcm.googleapis.com/fcm/send/example_subscription"
+            };
+            var payload = JsonConvert.SerializeObject(new
+            {
+                title = "Test Notification",
+                message = "This is a test message"
+            });
+
+            await _pushService.SendNotificationAsync(subscription, payload);
+            return Ok();
+        }
+    }
+}
