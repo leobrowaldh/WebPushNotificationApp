0ff838de75bbf6ec60217b75a8c1f243a1328d7e
leobrowaldh <leobrowaldh@gmail.com>
2024-10-01 12:58:32 +0200

    if the push is in string format, the sw will now recognice it as such

--- WebPushNotificationApp.Mvc/wwwroot/sw.js
+++ WebPushNotificationApp.Mvc/wwwroot/sw.js
@@ -1,8 +1,18 @@
 ﻿self.addEventListener('push', function (event) {
-    const data = event.data.json(); //parse the recieved JSON payload into a js object.
+    let data;
+
+    try {
+        // Attempt to parse the received data as JSON
+        data = event.data.json();
+    } catch (e) {
+        // If parsing fails, treat it as plain text
+        data = { title: 'Notification', message: event.data.text() };
+    }
+
     const options = {
         body: data.message,
     };
+
     event.waitUntil(
         self.registration.showNotification(data.title, options)
     );
