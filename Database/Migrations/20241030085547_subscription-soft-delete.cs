403f9c629937e1d03b1748adef762bfcafbf7ab5
leobrowaldh <leobrowaldh@gmail.com>
2024-10-30 09:56:16 +0100

    subscription soft delete migration run

---
+++ Database/Migrations/20241030085547_subscription-soft-delete.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class subscriptionsoftdelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Subscriptions",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Subscriptions");
+        }
+    }
+}
